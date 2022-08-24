ad762f8f9ccc10c8fef046368195ba806225802e
suyeon-jung <wjdjsy210@gmail.com>
2022-08-24 21:13:43 +0900

    [refactor] : 랜덤 이미지 생성 코드 수정 - default 이미지 생성을 위한 useEffect 처리 추가 - 이미지 불러오는동안 disable 처리 추가

--- src/pages/card/CardMaker.tsx
+++ src/pages/card/CardMaker.tsx
@@ -9,8 +9,9 @@ const CardMaker = () => {
 
   const { to, from, content } = cardForm;
 
-  const [fileImage, setFileImage] = useState("");
+  const [fileImage, setFileImage] = useState('');
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = e.target;
@@ -25,33 +26,52 @@ const CardMaker = () => {
     // 카드 생성 로직 구현 필요
   }
 
-  // 이미지 업로드
+  // 직접 이미지 업로드
   function handleUploadImage(e: React.ChangeEvent<HTMLInputElement>) {
     if (!e.target.files) {
       return;
     }
+    console.log(e.target.files);
     setFileImage(URL.createObjectURL(e.target.files[0]));
   }
 
-  // 랜덤 이미지 생성
-  function handleRandomImage() {
-    fetch('https://source.unsplash.com/random/960x600')
-    .then((response) => {
-      document.getElementById('previewImage')?.setAttribute('src', response.url);
-    })
-    
+  // 랜덤 이미지 업로드
+  async function handleRandomImage() {
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://source.unsplash.com/random/960x600');
+      setFileImage(response.url);
+    } catch (error) {
+      console.log(error);
+      setFileImage('');
+    }
+    setIsLoading(false);
+    // document.getElementById('previewImage')?.setAttribute('src', response.url);
   }
+
+  useEffect(() => {
+    handleRandomImage();
+  }, []);
 
   return (
     <div>
       카드 제작 페이지
       <form>
-        <img id="previewImage" src={fileImage}></img>
+        <img id="previewImage" src={fileImage} />
       </form>
       <form onSubmit={handleFormSubmit}>
-      <input name="picture" accept="image/*" ref={inputRef} onChange={handleUploadImage} type="file"></input>
-      <button onClick={handleRandomImage}>랜덤 이미지 생성</button>
-      <br/>
+        <input
+          name="picture"
+          accept="image/*"
+          ref={inputRef}
+          onChange={handleUploadImage}
+          type="file"
+          disabled={isLoading}
+        />
+        <button onClick={handleRandomImage} disabled={isLoading}>
+          랜덤 이미지 생성
+        </button>
+        <br />
         <input name="to" placeholder="To" type="text" value={to} onChange={handleInputChange} />
         <input name="from" placeholder="From" type="text" value={from} onChange={handleInputChange} />
         <input name="content" placeholder="Content" type="text" value={content} onChange={handleInputChange} />
